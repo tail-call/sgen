f40ed260a7c815d60ca8da321899abbf52b08277
T. C <infernalsaurian@gmail.com>
2016-07-21 10:15:43 +0600

    Initial commit

---
+++ popcorn.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+# Run like this:
+# $ python sngen.py 44000 | aplay -r 44000
+
+from sgen import *
+
+sq1 = SquareOsc(amplitude=0.3, rate=SAMPLE_RATE)
+m1 = melody([
+             note("G5"), 0,
+             note("F5"), 0,
+             note("G5"), 0,
+             note("D5"), 0,
+             note("Bb4"), 0,
+             note("D5"), 0,
+             note("G4"), 0,
+             0, 0,
+             note("G5"), 0,
+             note("F5"), 0,
+             note("G5"), 0,
+             note("D5"), 0,
+             note("Bb4"), 0,
+             note("D5"), 0,
+             note("G4"), 0,
+             0, 0,
+             note("G5"), 0,
+             note("A5"), 0,
+             note("Bb5"), 0,
+             note("A5"), 0,
+             note("Bb5"), 0,
+             note("G5"), 0,
+             note("A5"), 0,
+             note("G5"), 0,
+             note("A5"), 0,
+             note("F5"), 0,
+             note("G5"), 0,
+             note("D5"), 0,
+             note("Bb4"), 0,
+             note("D5"), 0,
+             note("G5"), 0,
+             0, 0,
+            ])
+sq2 = SawOsc(amplitude=0.2, rate=SAMPLE_RATE)
+m2 = melody([
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("A#2"), 0,
+             note("A#3"), 
+             note("F3"),
+             note("A#2"), 0,
+             note("A#3"), 
+             note("F3"),
+             note("A2"), 0,
+             note("A3"), 
+             note("F3"),
+             note("A2"), 0,
+             note("A3"), 
+             note("F3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+             note("G2"), 0,
+             note("G3"), 
+             note("D3"),
+            ])
+
+sq3 = NoiseOsc(amplitude=0.2, rate=SAMPLE_RATE)
+m3 = melody([
+             0, 0, note("G2"), 0,
+             0, 0, note("G2"), 0,
+            ])
+
+ticks = 0
+state = 18
+
+while True:
+    s1 = sq1.nextsample()
+    s2 = sq2.nextsample()
+    s3 = sq3.nextsample()
+
+    writesample(s1+s2+s3)
+
+    ticks = ticks + 1
+    if ticks > SAMPLE_RATE/150: # 150 times per second
+        ticks = 0
+        state = state + 1
+        sq1.amplitude = sq1.amplitude*0.7
+        sq2.amplitude = sq2.amplitude*0.95
+        sq3.amplitude = sq3.amplitude*0.8
+
+    if state > 17:
+        state = 0
+        sq1.amplitude = 0.3
+        sq1.frequency = next(m1)
+        sq2.amplitude = 0.2
+        sq2.frequency = next(m2)
+        sq3.amplitude = 0.2
+        sq3.frequency = next(m3)
---
+++ sgen.py
@@ -0,0 +1,119 @@
+import sys
+
+from math import floor
+from random import random 
+
+SAMPLE_RATE = int(sys.argv[1])
+
+def clamp(x):
+    """
+    Converts a real number x ∈ [-1.0, + 1.0] to an unsigned 8 bit integer
+    (i.e. (-128, 127) as (0, 255))
+    """
+    return floor((x+1.0)*128)
+
+
+class Osc:
+    """
+    A generic oscillator class.
+    """
+    def __init__(self, frequency=440, amplitude=1.0, phase = 0,
+                 rate=8000):
+        self.frequency = frequency
+        self.amplitude = amplitude
+        self.phase = phase
+        self.rate = rate
+
+
+    def incphase(self):
+        time_delta = self.frequency/self.rate
+        self.phase = (self.phase + time_delta) % 1
+        return time_delta
+
+
+    def nextsample(self):
+        sample = self.amplitude * self.oscillate()
+        self.incphase()
+        return sample
+
+
+    def oscillate(self):
+        """
+        A method to be defined in derived class.
+        """
+        pass
+
+
+class SquareOsc(Osc):
+    """
+    Square wave oscillator.
+    """
+    def oscillate(self):
+        if self.phase < 0.5:
+            return 1.0
+        else:
+            return -1.0
+
+
+class SawOsc(Osc):
+    """
+    Sawtooth wave oscillator.
+    """
+    def oscillate(self):
+        return self.phase * 2 - 1
+
+class NoiseOsc(Osc):
+    """
+    Noise generator.
+    """
+    def oscillate(self):
+        if self.frequency > 0:
+            return random()*2 - 1
+        else:
+            return 0
+
+
+def writesample(x):
+    sys.stdout.buffer.write(bytes([clamp(x)]))
+
+
+def note(symbol):
+    """
+    For a given note symbol of form [abcdefg](#|b)?[0-9] returns this
+    note's frequency.
+
+    E.g. note("A4") == 440,
+         note("C#3") == 138.59,
+         note("Ab5") == 830.61.
+
+    See <http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html> for details.
+    """
+    notes_offsets = { 'A': +0, 'B': +2, 'C': -9, 
+                      'D': -7, 'E': -5, 'F': -4,
+                      'G': -2 }
+    base = 440 # frequency of A4 is 110 Hz
+    root = 2**(1/12)
+
+    offset = notes_offsets[symbol[0].upper()]
+
+    symbol = symbol[1::]
+
+    if symbol[0] == "#":
+        offset += 1
+        symbol = symbol[1::]
+
+    if symbol[0] == "b":
+        offset -= 1
+        symbol = symbol[1::]
+
+    multiplier = int(symbol[0])
+
+    return base * root ** (offset - (4 - multiplier) * 12)
+
+
+def melody(notes):
+    i = 0
+
+    while True:
+        yield notes[i]
+        i = (i+1) % len(notes)
